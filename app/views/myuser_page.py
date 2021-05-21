6bf18924c0683517e2500e229ae1db8ac559a8f7
Alexander Ruberto <rubertoalexander@gmail.com>
2021-05-21 21:51:54 +1000

    Remove username from user URL

--- app/views/myuser_page.py
+++ app/views/myuser_page.py
@@ -7,11 +7,17 @@ from app import app
 #     username = "Testing"
 #     return render_template("myuserpage.html", username = username)
 
-@app.route("/myuserdetails_page/<string:username>")
-def myuserdetails_page(username):
+@app.route("/myuserdetails_page")
+def myuserdetails_page():
+    username = request.cookies.get("username")
+    if not username: redirect("/login")
+
     return render_template("myuserpage.html", username = username)
 
 
-@app.route("/changeaccountdetails_page/<string:username>")
-def changeaccountdetails_page(username):
+@app.route("/changeaccountdetails_page")
+def changeaccountdetails_page():
+    username = request.cookies.get("username")
+    if not username: redirect("/login")
+    
     return render_template("changeaccountdetailspage.html", username = username)

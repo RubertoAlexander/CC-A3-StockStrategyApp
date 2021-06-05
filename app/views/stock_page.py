419b156941dccf84c8c89f3e30b743ba0048c87c
Alexander Ruberto <rubertoalexander@gmail.com>
2021-06-05 18:31:48 +1000

    Stock details

--- app/views/stock_page.py
+++ app/views/stock_page.py
@@ -1,10 +1,17 @@
 from flask import render_template, request, redirect
 
 from app import app
+from app.models import yf_stock
 
-@app.route("/stock")
-def stock_page():
+@app.route("/stock/<symbol>")
+def stock_page(symbol: str):
     username = request.cookies.get("username")
     if not username: redirect("/login")
+
+    stock = {
+        "symbol": symbol,
+        "name": yf_stock.get_name(symbol),
+        "prices": yf_stock.get_prices(symbol)
+    }
         
-    return render_template("stock_page.html")
+    return render_template("stock_page.html", stock=stock)

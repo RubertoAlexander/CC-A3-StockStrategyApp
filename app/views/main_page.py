fcf292468426d09b3b29fac22f0b8a50c7ec3db0
Alexander Ruberto <rubertoalexander@gmail.com>
2021-06-06 20:08:01 +1000

    Basic favourites

--- app/views/main_page.py
+++ app/views/main_page.py
@@ -1,3 +1,4 @@
+from app.models import dynamodb_favourites
 from flask import render_template, request, redirect, make_response
 
 from app import app
@@ -5,9 +6,17 @@ from app import app
 @app.route("/")
 def main_page():
     username = request.cookies.get("username")
-    if not username: redirect("/login")
+    if not username:
+        return redirect("/login")
+
+    favourites = dynamodb_favourites.get_favourites(username)
+    print(favourites)
+    fav_stocks = []
+    for stock in favourites["stocks"]:
+        fav_stocks.append(stock[:3])
+    print(fav_stocks)
         
-    return render_template("mainpage.html", username=username)
+    return render_template("mainpage.html", username=username, favs=fav_stocks)
 
 @app.route("/logout")
 def logout():
--- app/views/stock_page.py
+++ app/views/stock_page.py
@@ -1,12 +1,12 @@
 from flask import render_template, request, redirect
 
 from app import app
-from app.models import yf_stock
+from app.models import dynamodb_favourites, yf_stock
 
 @app.route("/stock/<symbol>")
 def stock_page(symbol: str):
     username = request.cookies.get("username")
-    if not username: redirect("/login")
+    if not username: return redirect("/login")
 
     stock = {
         "symbol": symbol,
@@ -15,3 +15,13 @@ def stock_page(symbol: str):
     }
         
     return render_template("stock_page.html", stock=stock)
+
+@app.route("/fav/<symbol>")
+def add_favourite(symbol: str):
+    username = request.cookies.get("username")
+    favourites = dynamodb_favourites.get_favourites(username)
+    stocks = favourites["stocks"]
+    stocks.add(symbol+".AX")
+
+    result = dynamodb_favourites.put_favourites(username, stocks)
+    return redirect("/stock/"+symbol)

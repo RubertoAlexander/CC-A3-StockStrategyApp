b4dba428d4757c61e444a0d9836015a96c2d3931
Alexander Ruberto <rubertoalexander@gmail.com>
2021-06-13 19:35:32 +1000

    Auto stash before merge of "feature/strategy" and "develop"

--- app/views/builder_strategy_page.py
+++ app/views/builder_strategy_page.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, jsonify
 
 from app import app
-from app.models import dynamodb_favourites, yf_stock
+from app.models import dynamodb_favourites
 
-@app.route("/builder/strategy", methods=["GET", "POST"])
+@app.route("/builder", methods=["GET", "POST"])
 def builder_strategy_page():
     username = request.cookies.get("username")
     if not username: return redirect("/login")
@@ -11,4 +11,16 @@ def builder_strategy_page():
     favourites = dynamodb_favourites.get_favourites(username)
     print(favourites)
     
-    return render_template("builder_strategy.html", favs=favourites["stocks"], username=username)
+    return render_template("builder_strategy.html", favs=favourites["stocks"])
+
+@app.route("/builder/<stock>/save/<name>", methods=["POST"])
+def save_strategy(stock: str, name: str):
+    username = request.cookies.get("username")
+
+    timerange = request.json["timerange"]
+    interval = request.json["interval"]
+    buy_rules = request.json["indicator_buys"]
+    sell_rules = request.json["indicator_sells"]
+
+    print("saved")
+    
